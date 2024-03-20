f2644f0a28a68b0b880235d0ba9a82883317e502
lander <guevarralander0@gmail.com>
2024-03-21 02:45:28 +0800

    fix design

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,4 +1,4 @@
-import { Heading, Image, Box, Center, VStack } from "@chakra-ui/react";
+import { Heading, Box, Center, VStack, Text } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import Img1 from "../Images/IMG_2983.JPG";
 import Img2 from "../Images/IMG_2984.JPG";
@@ -12,36 +12,74 @@ import Img8 from "../Images/IMG_2990.JPG";
 function Hero() {
   const images = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8];
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const [hovered, setHovered] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImgIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 500); // Change image every 1 second (adjust as needed)
+    }, 500); // Change image every 0.5 second (adjust as needed)
 
     return () => clearInterval(interval);
   }, [images.length]);
 
+  const handleHover = () => {
+    setHovered(!hovered);
+  };
+
   return (
     <>
-      <VStack justifyContent="center" alignItems="center" bgColor="red">
-        <Box w="1440px" h="1024px" bgColor="blue">
-          <Heading
-            fontFamily="Black Ops One"
-            bgImage={images[currentImgIndex]}
-            bgSize="cover"
-            bgClip="text"
-            fontSize="500px"
-            fontWeight="bold"
-            color="transparent"
-            textAlign="center" // Center the text horizontally
-            mt="170px"
-            sx={{
-              backgroundSize: "10%", // Adjust the size as needed
-              backgroundPosition: "center", // Optional: Center the mask image
-            }}
-          >
-            X
-          </Heading>
+      <VStack justifyContent="center" alignItems="center">
+        <Box w="100%" h="1024px" bgColor="#717170">
+          <Center>
+            <Heading
+              style={{
+                cursor: hovered ? "arrow" : "default",
+                backgroundSize: hovered ? "100% 100%" : "45%",
+                backgroundPosition: hovered ? "center" : "center",
+              }}
+              fontFamily={hovered ? "Black Ops One" : "Black Ops One"}
+              bgImage={hovered ? "" : images[currentImgIndex]}
+              bgSize={hovered ? "auto" : "cover"}
+              bgClip="text"
+              fontSize={hovered ? "250px" : "500px"}
+              fontWeight="semi-bold"
+              color={hovered ? "#fcfcfc" : "transparent"}
+              textAlign="center"
+              mt={hovered ? "270px" : "170px"}
+              transition="background-size 1s, color 3s"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+            >
+              {hovered ? (
+                <>
+                  LANDER
+                  <Text
+                    textAlign="justify"
+                    fontSize={hovered ? "18px" : "18px"}
+                    px="100px"
+                    fontFamily="Bebas Neue, sans-serif"
+                    style={{
+                      textAlign: "center",
+                      wordSpacing: "5px",
+                      lineHeight: "26px",
+                    }}
+                    letterSpacing="4px"
+                  >
+                    Welcome to my digital playground! 🚀 Where bytes meet
+                    brilliance, I'm a code-slinging Computer Science student at
+                    Laguna University, deep-diving into my fourth year of
+                    college. With a heart that beats in code and a mind wired
+                    for innovation, I'm here to share my passion for
+                    programming. Join me in unlocking the magic of code as we
+                    journey through the ever-evolving landscape of technology.
+                    Let's turn lines of code into works of art together! 💻✨
+                  </Text>
+                </>
+              ) : (
+                "X"
+              )}
+            </Heading>
+          </Center>
         </Box>
       </VStack>
     </>
